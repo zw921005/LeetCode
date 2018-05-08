a6dcd2722951ec64bbdfc1de8e37717c18d69ce8
Konan.Zheng <konan.zheng@searching-info.com>
2018-05-08 17:21:01 +0800

    2018/05/08

---
+++ LeetCode/LC53.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class lc53
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            if (nums.AsEnumerable().All(i => i < 0))
+            {
+                return nums.Max();
+            }
+
+            int maxSum = 0, sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+                else if (sum < 0)
+                {
+                    sum = 0;
+                }
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ LeetCode/lc121.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class lc121
+    {
+        static void Main(string[] args)
+        {
+            //int[] input = { 7, 1, 5, 3, 6, 4 };
+            int[] input = { 2, 6, 1, 4 };
+            int output = MaxProfit(input);
+            Console.WriteLine(output);
+            Console.Read();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            if (prices.Length > 0)
+            {
+                int min = prices[0];
+                for (int i = 1; i < prices.Length; i++)
+                {
+                    if (prices[i] < min)
+                    {
+                        min = prices[i];
+                    }
+                    else if (prices[i] - min > max)
+                    {
+                        max = prices[i] - min;
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        //public static int MaxProfit(int[] prices)
+        //{
+        //    int max = 0;
+        //    for (int i = 0; i < prices.Length; i++)
+        //    {
+        //        for (int j = i; j < prices.Length; j++)
+        //        {
+        //            if (prices[j] - prices[i] > max)
+        //            {
+        //                max = prices[j] - prices[i];
+        //            }
+        //        }
+        //    }
+        //    return max;
+        //}
+    }
+}
