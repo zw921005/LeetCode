a7f2188ac47362b60b7f5c092dde3eed79b71888
Konan.Zheng <konan.zheng@searching-info.com>
2020-07-17 17:38:41 +0800

    739

---
+++ LeetCode/Code/lc739.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class lc739
+    {
+        public int[] dailyTemperatures(int[] T)
+        {
+            int[] result = new int[T.Length];
+            int[] location = new int[101];
+
+            for (int i = T.Length - 1; i >= 0; i--) {
+                for (int j = T[i] + 1; j <= 100; j++) {
+                    if (location[j] != 0) {
+                        result[i] = location[j] - i;
+                        break;
+                    }
+                }
+                location[T[i]] = i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Code/lc739Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code.Tests
+{
+    [TestClass()]
+    public class lc739Tests
+    {
+        [TestMethod()]
+        public void dailyTemperaturesTest()
+        {
+            var obj = new lc739();
+            obj.dailyTemperatures(new int[] { 73, 74, 75, 71, 69, 72, 76, 73 });
+        }
+    }
+}
