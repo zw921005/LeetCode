e3b789be371319331ddf5016225c8b04cfbe73a9
Konan.Zheng <496706477@qq.com>
2020-07-23 21:46:04 +0800

    64

---
+++ LeetCodeTests/Code/lc064Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code.Tests
+{
+    [TestClass()]
+    public class lc064Tests
+    {
+        [TestMethod()]
+        public void MinPathSumTest()
+        {
+            int[][] grid = new int[][] {
+                new int[]{1,3,1},
+                new int[]{1,5,1},
+                new int[]{4,2,1}
+            };
+
+            var obj = new lc064();
+            if (obj.MinPathSum(grid) != 7) {
+                Assert.Fail();
+            }
+        }
+    }
+}
