0cd38520730b8dd40be727f3e7b8704d0590a86a
Konan.Zheng <konan.zheng@searching-info.com>
2020-07-24 17:50:09 +0800

    1025

---
+++ LeetCode/Code/lc1025.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class lc1025
+    {
+        public bool DivisorGame(int N)
+        {
+            bool[] result = new bool[N + 1];
+
+            result[1] = false;
+            for (int i = 2; i <= N; i++) {
+                var list = new HashSet<int>();
+                list.Add(1);
+                for (int j = 2; j * j <= i; j++) {
+                    if (i / j * j == i) {
+                        list.Add(j);
+                        list.Add(i / j);
+                    }
+                }
+
+
+                foreach (var index in list) {
+                    if (result[i - index] == false) {
+                        result[i] = true;
+                    }
+                }
+            }
+
+            return result[N];
+        }
+
+        public bool DivisorGame1(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
---
+++ LeetCodeTests/Code/lc1025Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code.Tests
+{
+    [TestClass()]
+    public class lc1025Tests
+    {
+        [TestMethod()]
+        public void DivisorGameTest()
+        {
+            var obj = new lc1025();
+            obj.DivisorGame(4);
+            Assert.Fail();
+        }
+    }
+}
