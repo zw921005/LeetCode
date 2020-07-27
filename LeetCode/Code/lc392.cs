6e4ea8f6ba7657fca60d897a0219902a48087a88
Konan.Zheng <konan.zheng@searching-info.com>
2020-07-27 18:34:18 +0800

    392

---
+++ LeetCode/Code/lc392.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    class lc392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int p1 = 0; int p2 = 0;
+
+            while (p1 < s.Length && p2 < t.Length) {
+                if (s[p1] == t[p2]) {
+                    p1++; p2++;
+                }
+                else {
+                    p2++;
+                }
+            }
+
+            return p1 == s.Length;
+        }
+    }
+}
