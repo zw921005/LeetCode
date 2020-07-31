c1b51e11c43469798bae5bfb08ef07978288df86
Konan.Zheng <konan.zheng@searching-info.com>
2020-07-31 16:07:44 +0800

    1689

---
+++ LeetCode/Code/lc1689.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    class lc1689
+    {
+        public int FindMagicIndex(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++) {
+                if (nums[i] == i)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
