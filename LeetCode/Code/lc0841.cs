90fd28b6277e6fbee18d4dfac754218c9c770de5
Konan.Zheng <konan.zheng@searching-info.com>
2020-08-31 10:28:00 +0800

    /

---
+++ LeetCode/Code/lc0841.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    class lc0841
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var visited = new HashSet<int>();
+            visited.Add(0);
+
+            var queue = new Queue<int>();
+            queue.Enqueue(0);
+
+            while (queue.Count > 0) {
+                var room = queue.Dequeue();
+                foreach(var r in rooms[room]) {
+                    if (!visited.Contains(r)) {
+                        visited.Add(r);
+                        queue.Enqueue(r);
+                    }
+                }
+            }
+
+            return rooms.Count == visited.Count();
+        }
+    }
+}
