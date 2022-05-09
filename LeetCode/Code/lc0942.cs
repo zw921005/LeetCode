f7189988b931dc438a2370d226d6925d58d29be4
Konan.Zheng <konan.zheng@searching-info.com>
2022-05-09 13:44:05 +0800

    /

---
+++ LeetCode/Code/lc0942.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class lc0942
+    {
+        public int[] DiStringMatch(string s)
+        {
+            var result = new int[s.Length + 1];
+            result[0] = 0;
+            var i = 1;
+            var top = 0; var bottom = 0;
+            foreach (char c in s)
+            {
+                if (c == 'I')
+                {
+                    result[i++] = ++top;
+                }
+                else if (c == 'D')
+                {
+                    result[i++] = --bottom;
+                }
+            }
+
+            for (int j = 0; j < result.Length; j++)
+            {
+                result[j] -= bottom;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Code/lc0942Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code.Tests
+{
+    [TestClass()]
+    public class lc0942Tests
+    {
+        [TestMethod()]
+        public void DiStringMatchTest()
+        {
+            var obj = new lc0942();
+            Console.WriteLine(String.Join(",", obj.DiStringMatch("IDID")));
+            Console.WriteLine(String.Join(",", obj.DiStringMatch("III")));
+            Console.WriteLine(String.Join(",", obj.DiStringMatch("DDI")));
+        }
+    }
+}
