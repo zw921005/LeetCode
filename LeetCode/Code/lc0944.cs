7e456c345f4d886d6f1172554380d873faa1b2b1
Konan.Zheng <konan.zheng@searching-info.com>
2022-05-12 16:55:07 +0800

    944

---
+++ LeetCode/Code/lc0944.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class lc0944
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int result = 0;
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = 0; j < strs.Length - 1; j++)
+                {
+                    if (strs[j][i] > strs[j + 1][i])
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Code/lc0944Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code.Tests
+{
+    [TestClass()]
+    public class lc0944Tests
+    {
+        [TestMethod()]
+        public void MinDeletionSizeTest()
+        {
+            var obj = new lc0944();
+            Assert.AreEqual(obj.MinDeletionSize(new string[] { "cba", "daf", "ghi" }), 1);
+            Assert.AreEqual(obj.MinDeletionSize(new string[] { "a", "b" }), 0);
+            Assert.AreEqual(obj.MinDeletionSize(new string[] { "zyx", "wvu", "tsr" }), 3);
+        }
+    }
+}
