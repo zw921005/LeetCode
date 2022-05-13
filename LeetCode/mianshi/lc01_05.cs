7cff23d0552fa646eea1a15886eea06167fda015
Konan.Zheng <konan.zheng@searching-info.com>
2022-05-13 10:14:21 +0800

    01_05

---
+++ LeetCode/mianshi/lc01_05.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mianshi
+{
+    public class lc01_05
+    {
+        public bool OneEditAway(string first, string second)
+        {
+            if (first.Length < second.Length)
+            {
+                return OneEditAway(second, first);
+            }
+
+            int count = 0;
+            if (first.Length > second.Length)
+            {
+                count++;
+                int i = 0;
+                for (i = 0; i < first.Length; i++)
+                {
+                    if (i == second.Length || first[i] != second[i])
+                    {
+                        break;
+                    }
+                }
+                first = first.Substring(0, i) + first.Substring(i + 1, first.Length - i - 1);
+            }
+
+            if (first.Length != second.Length)
+                return false;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    count++;
+                }
+            }
+
+            return count <= 1;
+        }
+
+    }
+}
---
+++ LeetCodeTests/mianshi/lc01_05Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.mianshi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mianshi.Tests
+{
+    [TestClass()]
+    public class lc01_05Tests
+    {
+        [TestMethod()]
+        public void OneEditAwayTest()
+        {
+            var obj = new lc01_05();
+            Assert.AreEqual(obj.OneEditAway("teacher", "attacher"), false);
+            Assert.AreEqual(obj.OneEditAway("pale", "ple"), true);
+            Assert.AreEqual(obj.OneEditAway("pales", "pal"), false);
+        }
+    }
+}
